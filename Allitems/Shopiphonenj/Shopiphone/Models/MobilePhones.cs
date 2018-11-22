a6ecd5fbfc71bd821d732cc45f4ce38786f97745
AliaJafri <alialiaqat250@gmail.com>
2018-11-22 10:55:57 +0000

    all items

---
+++ Allitems/Shopiphonenj/Shopiphone/Models/MobilePhones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopiphone.Models
+{
+    public class MobilePhones
+    {
+        private string iME;
+        private string brand;
+        private string price;
+        private string description;
+        private string warranty;
+        private string model;
+        private string picture;
+        public static string type;
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        public string IME { get => iME; set => iME = value; }
+        public string Brand { get => brand; set => brand = value; }
+
+        public string Description { get => description; set => description = value; }
+        public string Warranty { get => warranty; set => warranty = value; }
+        public string Model { get => model; set => model = value; }
+        public string Picture { get => picture; set => picture = value; }
+        public string Price { get => price; set => price = value; }
+       
+    }
+}
