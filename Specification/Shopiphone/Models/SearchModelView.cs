f49a88a202776dc1cd0bc210e47c2b946d77c508
Bisma-Amir <38394856+Bisma-Amir@users.noreply.github.com>
2018-12-04 11:12:11 +0500

    specific detail of a phone

---
+++ Specification/Shopiphone/Models/SearchModelView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopiphone.Models
+{
+    public class SearchModelView
+    {
+        public string type { get; set; }
+    }
+}
